9ae07df3982ee044fada04bbb5f8a7f5b99af66a
Juhniel <juhn.sweden@gmail.com>
2023-06-09 13:30:41 +0200

    init

---
+++ src/main/java/com/juhnkim/projectclassroom/ProjectClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.juhnkim.projectclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectClassroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhnkim/projectclassroom/ProjectClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhnkim.projectclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
